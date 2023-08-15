0c8e0911e9f2bef2441570da9f969b62803c3d2b
maddie-newby <madison1999@hotmail.co.uk>
2023-08-15 13:41:44 +0100

    Add competitions as list

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg fixed-top">
+    <nav className="navbar navbar-expand-lg">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">
           Home
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,6 +5,19 @@ const About = () => {
   return (
     <div className="about">
       <header>About Page</header>
+      <p>
+        Cheerleading in the UK is becoming more and more popular each year. To
+        compensate for this, more competitions and more teams are becoming
+        available. As a result, there is so much information flying around and
+        it is never all in one place. Therefore, I have trolled through lots of
+        different websites, Facebook pages, Instagram pages, and more, to
+        provide a website that has all current information so that it is easily
+        accessible for everyone.
+      </p>
+      <p>
+        If there is anything missing or anything that is incorrect, please so
+        let me know!
+      </p>
     </div>
   );
 };
